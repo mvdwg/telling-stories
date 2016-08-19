93b1d4f3078a960d54e0817df6cbfa0bac3806bb
Marcelo Dominguez <marcelo@mimiquate.com>
2016-08-18 23:30:39 -0300

    Add a simple index page of all the acceptance tests

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 /* jshint node: true */
 'use strict';
 var EngineAddon = require('ember-engines/lib/engine-addon');
+var AcceptanceTestFilter = require('./lib/acceptance-test-filter');
+
+var fs = require('fs');
 
 module.exports = EngineAddon.extend({
-  name: 'telling-stories'
+  name: 'telling-stories',
+
+  included: function(app) {
+    this._super.included.apply(this, arguments);
+  },
+
+  preprocessTree: function(type, tree) {
+    if (type === "test") {
+      return new AcceptanceTestFilter(tree);
+    }
+    return tree;
+  }
 });
