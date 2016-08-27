cff784d00b60242d97a6347c906ba95f51c29c2b
Santiago Ferreira <san650@gmail.com>
2016-08-26 22:15:24 -0300

    Update `module-for-acceptance` to add custom hook

--- blueprints/telling-stories/index.js
+++ blueprints/telling-stories/index.js
@@ -1,10 +1,20 @@
 /*jshint node:true*/
+
+var existsSync = require('exists-sync');
+
 module.exports = {
   description: 'Install telling-stories dependencies',
 
   normalizeEntityName: function() {},
 
   afterInstall: function() {
+    // Register shutdown animation to the end of every acceptance test
+    if (existsSync('tests/helpers/module-for-acceptance.js')) {
+      this.insertIntoFile('tests/helpers/module-for-acceptance.js', "      afterEach = window.require('telling-stories').shutdown(afterEach);", {
+        after: "let afterEach = options.afterEach && options.afterEach.apply(this, arguments);\n"
+      });
+    }
+
     return this.addAddonsToProject({
       packages: [
         { name: 'ember-cli-page-object', version: '^1.6.0' },
