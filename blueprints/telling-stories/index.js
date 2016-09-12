7751f27ae78a07efdbd8d84fb09b9a39fd67edf6
Santiago Ferreira <san650@gmail.com>
2016-09-11 23:14:00 -0300

    Update dashboard

--- blueprints/telling-stories/index.js
+++ blueprints/telling-stories/index.js
@@ -18,7 +18,7 @@ module.exports = {
     return this.addAddonsToProject({
       packages: [
         { name: 'ember-cli-page-object', version: '^1.6.0' },
-        { name: 'telling-stories-dashboard', version: '1.0.0-alpha.1' }
+        { name: 'telling-stories-dashboard', version: '1.0.0-alpha.2' }
       ]
     });
   }
